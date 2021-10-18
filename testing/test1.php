45e526eb23968b66cb453e21a37e6b7c3185081f
rohith donthula <19rj1a0557@gmail.com>
2021-10-18 17:39:14 +0530

    my first page

---
+++ testing/test1.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<?php
+//Assing variables and set empty 
+$x =$_POST["1st"];
+
+$y =$_POST ["2nd"];
+
+echo $x,$_POST["sig"],$y;//addition
+echo"<br>";
+/* echo $x $_POST["sig"] $y;//subtraction 
+echo"<br>";
+echo $x $_POST["sig"]$y;//multiplication 
+echo"<br>";
+echo $x$_POST["sig"]$y;//division 
+echo"<br>";
+echo $x$_POST["sig"] $y;//modulus 
+echo"<br>"; */
+//echo $x,$_POST["sig" ],$y;//exponentiation echo"<br>";
+?>
+</body>
+</html>
